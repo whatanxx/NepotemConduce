74e744e2b69e9d424a0500b10ea623943af3b19e
RadoslawWozny <wrx88023@student.wroclaw.merito.pl>
2025-03-12 19:45:19 +0100

    start MVC
    odpalilem jako serwer, stworzylo domyslna baze danych, zrobilem jakis podstawowy controller, wszystko dziala ale widzi tylko projekt, nie aplikacje ktora zrobilem z "python manage.py startapp Apka"

--- backend/nepotemconduce/settings.py
+++ backend/nepotemconduce/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # 'Apka.apps.ApkaConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
