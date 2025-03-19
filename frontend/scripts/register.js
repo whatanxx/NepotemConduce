14ba708cfac6b8fad6d21a2d53dcb76dabe511e1
Aleks Brzęczkowski <whatanxx@gmail.com>
2025-03-19 10:43:20 +0100

    Rejestracja i logowanie

--- frontend/scripts/register.js
+++ frontend/scripts/register.js
@@ -8,37 +8,37 @@ let voluntaryBtnClicked = false;
 
 let registerBtnEl = document.getElementById("register-btns");
 
-// seniorBtn.addEventListener("click", ()=>{
-//     voluntaryForm.style.display="";
-//     seniorBtnClicked = !seniorBtnClicked;
-//     voluntaryBtnClicked = false;
-//     console.log(seniorBtnClicked);
+seniorBtn.addEventListener("click", ()=>{
+    voluntaryForm.style.display="";
+    seniorBtnClicked = !seniorBtnClicked;
+    voluntaryBtnClicked = false;
+    console.log(seniorBtnClicked);
     
-//     if(seniorBtnClicked == true){
-//         seniorForm.style.display="flex";
-//         seniorForm.style.flexDirection="column";
-//         seniorForm.style.gap="16px"
-//         seniorForm.scrollIntoView();
-//     }
-//     else{
-//         seniorForm.style.display="";
+    if(seniorBtnClicked == true){
+        seniorForm.style.display="flex";
+        seniorForm.style.flexDirection="column";
+        seniorForm.style.gap="16px"
+        seniorForm.scrollIntoView();
+    }
+    else{
+        seniorForm.style.display="";
         
-//     }
-// } )
+    }
+} )
 
-// voluntaryBtn.addEventListener("click", ()=>{
-//     voluntaryBtnClicked = !voluntaryBtnClicked;
-//     seniorForm.style.display="";
-//     seniorBtnClicked = false;
-//     console.log(voluntaryBtnClicked);
-//     if(voluntaryBtnClicked == true){
-//         voluntaryForm.style.display="flex";
-//         voluntaryForm.style.flexDirection="column";
-//         voluntaryForm.style.gap="16px"
-//         voluntaryForm.scrollIntoView();
-//     }
-//     else{
-//         voluntaryForm.style.display="";
+voluntaryBtn.addEventListener("click", ()=>{
+    voluntaryBtnClicked = !voluntaryBtnClicked;
+    seniorForm.style.display="";
+    seniorBtnClicked = false;
+    console.log(voluntaryBtnClicked);
+    if(voluntaryBtnClicked == true){
+        voluntaryForm.style.display="flex";
+        voluntaryForm.style.flexDirection="column";
+        voluntaryForm.style.gap="16px"
+        voluntaryForm.scrollIntoView();
+    }
+    else{
+        voluntaryForm.style.display="";
         
-//     }
-// })
+    }
+})
