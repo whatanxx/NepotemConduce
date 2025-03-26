f4fe4010c52127c517095d5aef267561ea3b6716
Aleks Brzęczkowski <whatanxx@gmail.com>
2025-03-26 10:35:35 +0100

    Zmienilem strukture projektu. Dostosowalem go do dynamicznego generowania tresci przez Django.

--- backend/board/views.py
+++ backend/board/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(req):
+    return render(req, "board/index.html")
+
+def register(req):
+    return render(req, "board/register.html")
+
+def login(req):
+    return render(req, "board/login.html")
