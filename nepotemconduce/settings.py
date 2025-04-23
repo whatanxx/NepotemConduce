45191831eb8fdeaca9b7841382712563e0a14a4c
Aleks Brzęczkowski <whatanxx@gmail.com>
2025-04-23 11:39:36 +0200

    Added key

--- nepotemconduce/settings.py
+++ nepotemconduce/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +21,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!9m(npl20&ipc_1dry0%w^2rb15tb2*g@y2&m(oz&+=o!lljm&'
+SECRET_KEY = os.environ['SECRET_KEY']
+
+# SECRET_KEY = 'django-insecure-!9m(npl20&ipc_1dry0%w^2rb15tb2*g@y2&m(oz&+=o!lljm&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+
 
 
 # Application definition
@@ -129,3 +132,9 @@ STATICFILES_DIRS=[
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = '/'
+
+ALLOWED_HOSTS = [
+    os.environ.get('WEBSITE_HOSTNAME', 'localhost'),
+    'www.nepotemconduce-d4ahhtf3atenh2fr.westeurope-01.azurewebsites.net',
+    'nepotemconduce-d4ahhtf3atenh2fr.westeurope-01.azurewebsites.net'
+]
