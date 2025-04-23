31db0348e5d9c6d9966092302a727c0e0ba37a8a
Aleks Brzęczkowski <whatanxx@gmail.com>
2025-04-23 12:19:28 +0200

    TYts

--- nepotemconduce/settings.py
+++ nepotemconduce/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = "haklz@@e0092lhxc1u$u_@3zf53%_o5r(=spi2+l-szy!1pt=&"
 
 # SECRET_KEY = 'django-insecure-!9m(npl20&ipc_1dry0%w^2rb15tb2*g@y2&m(oz&+=o!lljm&'
 
